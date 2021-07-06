c7e30bdd7ffbc6f09066b5f1281562834c2cebb2
SumaiyaTajrin <tamanna17103181@gmail.com>
2021-07-06 13:34:49 +0600

    first commit

---
+++ xampp/htdocs/gymsystem/gymsystem.sql
@@ -0,0 +1,148 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Nov 23, 2017 at 08:01 AM
+-- Server version: 10.1.28-MariaDB
+-- PHP Version: 7.1.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `gymsystem`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `member`
+--
+
+CREATE TABLE `member` (
+  `fname` varchar(40) NULL,
+  `lname` varchar(40) NULL,
+  `email` varchar(40) NULL,
+  `contact` varchar(40) NULL,
+  `trainer` varchar(60) NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `doctorapp`
+--
+
+
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `logintb`
+--
+
+CREATE TABLE `logintb` (
+  `username` varchar(40) NOT NULL,
+  `password` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `logintb`
+--
+
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Package`
+--
+
+CREATE TABLE `Package` (
+  `Package_id` varchar(40) NOT NULL,
+  `Package_name` varchar(40) NOT NULL,
+  `Amount` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `Package`
+--
+
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Payment`
+--
+
+CREATE TABLE `Payment` (
+  `Payment_id` int(10) NOT NULL,
+  `Amount` int(20) NOT NULL,
+  `customer_id` varchar(20) NOT NULL,
+  `payment_type` varchar(20) NOT NULL
+  
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `Payment`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Trainer`
+--
+
+CREATE TABLE `Trainer` (
+  `Trainer_id` int(20) NOT NULL,
+  `Name` varchar(40) NOT NULL,
+  `phone` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `Trainer`
+--
+
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `doctorapp`
+--
+ALTER TABLE `member`
+  ADD PRIMARY KEY (`contact`);
+
+--
+-- Indexes for table `Package`
+--
+ALTER TABLE `Package`
+  ADD PRIMARY KEY (`Package_id`);
+
+--
+-- Indexes for table `Payment`
+--
+ALTER TABLE `Payment`
+  ADD PRIMARY KEY (`Payment_id`);
+
+--
+-- Indexes for table `Trainer`
+--
+ALTER TABLE `Trainer`
+  ADD PRIMARY KEY (`Trainer_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
